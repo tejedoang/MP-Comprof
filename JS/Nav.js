642cd13da22be76ff7aa55104a7fc1674e1481ba
Steven Tjiawi <Steven.tjiawi@student.umn.ac.id>
2022-08-08 14:03:36 +0700

    JS

--- JS/Nav.js
+++ JS/Nav.js
@@ -1,3 +1,22 @@
-$( document ).ready(function() {
-  console.log( "ready!" );
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".navbar-fixed-top");
+    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+  });
 });
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if($(window).scrollTop() > $(window).height()){
+          $(".menu").css({"bg.transparent":"nav-color"});   
+      }
+      else{
+          $(".menu").css({"nav-color":"bg.transparent"});
+      }
+
+  })
+})
+
+$(window).scroll(function(){
+  $('nav').toggleClass('scrolled', $(this).scrollTop() > 500);
+  });
